17ee12c8a5df684b532bb540a3fdace4353b1721
koiclone <cheryljcruz1@gmail.com>
2022-04-03 18:53:54 -0500

    included packages for application and first question prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// include project title, sections: description, table of contents, installation, usage, license, contributions, tests, and questions
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the title of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a project title!');
+                    return false;
+                }
+            }
+        }
+    ])
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+questions().then(answers => console.log(answers));
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
